8c1843188ca3c73ec94db2be82bf9ec3f1d3d8ba
Minxyzgo <71958008+Minxyzgo@users.noreply.github.com>
2020-09-28 21:26:38 +0800

    Create Main.java

---
+++ src/minxyzgo/Main.java
@@ -0,0 +1,16 @@
+package minxyzgo;
+
+import mindustry.mod.*;
+
+public class Main extends Mod{
+
+    public Main(){
+         
+    }
+
+    @Override
+    public void loadContent(){
+	
+    }
+
+}
